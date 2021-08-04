2f9b16365d5380a6ec1dcc2d808b81eae04ef880
Kristoffer Hedstrom <kris.hedstrom@gmail.com>
2021-08-04 17:13:06 +0200

    cleanup

--- src/code.ts
+++ src/code.ts
@@ -4,19 +4,6 @@ figma.showUI(__html__, { height: 500 });
 
 figma.ui.onmessage = (msg) => {
   const { data, type } = msg;
-  if (type === "create-rectangles") {
-    const nodes: SceneNode[] = [];
-    for (let i = 0; i < msg.count; i++) {
-      const rect = figma.createRectangle();
-      rect.x = i * 150;
-      rect.fills = [{ type: "SOLID", color: { r: 1, g: 0.5, b: 0 } }];
-      figma.currentPage.appendChild(rect);
-      nodes.push(rect);
-    }
-    figma.currentPage.selection = nodes;
-    figma.viewport.scrollAndZoomIntoView(nodes);
-  }
-
   if (type === "updateTextStyles") {
     updateTextStyles(data);
   }
